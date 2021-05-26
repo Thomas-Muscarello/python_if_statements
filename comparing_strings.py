d9fe683811c0f636756a46906519126312e27806
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-05-26 13:33:00 -0400

    comparing strings

---
+++ comparing_strings.py
@@ -0,0 +1,11 @@
+#Comparing Numbers
+not_dog = "This is not a dog"
+
+def animal(x):
+    #Use a comparison operator to print the largest number.
+    if x == "dog":
+        return x
+    else:
+        return not_dog
+
+print(animal("cat"))
